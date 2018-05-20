a7f6dab1c975f94409b4b4b0e22ce1d4d3013335
Гурьев Василий Александрович <Vaschudo@gmail.com>
2018-05-20 04:12:09 +0300

    Make some fix

---
+++ Lesson5.fs
@@ -0,0 +1,33 @@
+﻿let checkToCorrect =
+    let getOpenBr c =
+        match c with
+        | ')' -> '('
+        | '}' -> '{'
+        | ']' -> '['
+
+    let rec checkToCorrectRec (stack : List<char>) (str : string) =
+        match (str.Length, List.length stack) with
+        | (0, 0) -> true
+        | (0, _) -> false
+        | (_, _) ->
+            match str.[0] with
+            | c when (c = '(') || (c = '{') || (c = '[') -> checkToCorrectRec (c::stack) (str.Substring(1))
+            | c when (c = ')') || (c = '}') || (c = ']') -> if List.length stack = 0 || List.head stack <> (getOpenBr c) then false else checkToCorrectRec (List.tail stack) (str.Substring(1))
+            | _ -> false
+    checkToCorrectRec []
+
+module pointFreeFunc =
+    let func x l = 
+        List.map (fun y -> y * x) l
+    let func'1 x =
+        List.map (fun y -> y * x)
+    let func'2 x =
+        List.map (fun y -> (*) x y)
+    let func'3 x =
+        List.map ((*) x)
+    let func'4 =
+        List.map << (*)
+
+[<EntryPoint>]
+let main argv =
+    0
---
+++ Lesson5Test/Test.fs
@@ -0,0 +1,45 @@
+﻿open NUnit.Framework 
+open FsUnit
+open FsCheck
+open Lesson5
+
+open System
+
+[<Test>]
+let ``test of check to correct bracket sequence 1`` () =
+    checkToCorrect "(({}[({}[])]))" |> should be True
+
+[<Test>]
+let ``test of check to correct bracket sequence 2`` () =
+    checkToCorrect "(){}[({}[])]))" |> should be False
+
+[<Test>]
+let ``test of check to correct bracket sequence 3`` () =
+    checkToCorrect "}]))}[({}[])]))" |> should be False
+
+[<Test>]
+let ``test of check to correct bracket sequence 4`` () =
+    checkToCorrect "" |> should be True
+
+[<Test>]
+let ``test of check to correct bracket sequence 5`` () =
+    checkToCorrect "{Hi!}" |> should be False
+
+[<Test>]
+let ``test of point free func`` () =
+    let checkFunc x l = 
+        if ((pointFreeFunc.func x l) = (pointFreeFunc.func'4 x l))
+        then true
+        else failwith ("Bad arguments: " + x.ToString() + " " + l.ToString())
+    Check.Quick checkFunc
+
+
+[<EntryPoint>]
+let main argv =
+    ``test of check to correct bracket sequence 1`` ()
+    ``test of check to correct bracket sequence 2`` ()
+    ``test of check to correct bracket sequence 3`` ()
+    ``test of check to correct bracket sequence 4`` ()
+    ``test of check to correct bracket sequence 5`` ()
+    ``test of point free func`` ()
+    0 
